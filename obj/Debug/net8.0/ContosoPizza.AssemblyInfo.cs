36e9f61df3fe6a8cce3bd2eb8087abd47e704da9
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2024-05-29 12:22:21 -0400

    using httprepl to test

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c059399985be67ca53a6aeda9a242f2d786e1a91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+892fb0d3654b9d3e1e0fd71ab9ff90b53a79e3a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
